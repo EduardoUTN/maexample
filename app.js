9783fb4bb62400c9fea594c7e2be7338539c8998
Eduardo Ortiz <eduortiz.g@gmail.com>
2015-11-17 10:54:00 -0600

    Added the increment Upvotes method, no empty post prevention

--- app.js
+++ app.js
@@ -12,6 +12,13 @@ app.controller('MainCtrl', [
       {title: 'post 5', upvotes: 36}
     ];
     $scope.addPost = function(){
-      $scope.posts.push({title: 'A new Post!', upvotes: 0});
+      if(!scope.title || $scope.title === ''){
+        return;
+      }
+      $scope.posts.push({title: $scope.title, upvotes: 0});
+      $scope.title = '';
     };
+    $scope.incrementUpvotes = function(post) {
+      post.upvotes += 1;
+    }
   }]);
