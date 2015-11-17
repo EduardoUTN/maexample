81e3cfe50ea1721523e8496400a89e0f1fd9e8cb
Eduardo Ortiz <eduortiz.g@gmail.com>
2015-11-17 14:45:41 -0600

    Added link attribute and field form

--- app.js
+++ app.js
@@ -10,13 +10,16 @@ app.controller('MainCtrl', [
       {title: 'post 3', upvotes: 11},
       {title: 'post 4', upvotes: 41},
       {title: 'post 5', upvotes: 36}
-    ];
+    ]
     $scope.addPost = function(){
-      if(!scope.title || $scope.title === ''){
-        return;
-      }
-      $scope.posts.push({title: $scope.title, upvotes: 0});
+      if(!$scope.title || $scope.title === ''){ return; }
+      $scope.posts.push({
+        title: $scope.title,
+        link: $scope.link,
+        upvotes: 0
+      });
       $scope.title = '';
+      $scope.link = '';
     };
     $scope.incrementUpvotes = function(post) {
       post.upvotes += 1;
