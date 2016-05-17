90b487a16c8f20f0244581c3ac85ff7058b4354c
Eduardo Ortiz <eduortiz.g@gmail.com>
2016-05-17 15:41:56 -0500

    Added Angular service

--- app.js
+++ app.js
@@ -1,16 +1,10 @@
 var app = angular.module('maNews', []);
 
 app.controller('MainCtrl', [
-  '$scope',
-  function($scope){
+  '$scope', 'posts',
+  function($scope, posts){
     $scope.test = 'Hello World';
-    $scope.posts = [
-      {title: 'post 1', upvotes: 23},
-      {title: 'post 2', upvotes: 55},
-      {title: 'post 3', upvotes: 11},
-      {title: 'post 4', upvotes: 41},
-      {title: 'post 5', upvotes: 36}
-    ]
+    $scope.posts = posts.posts;
     $scope.addPost = function(){
       if(!$scope.title || $scope.title === ''){ return; }
       $scope.posts.push({
@@ -25,3 +19,14 @@ app.controller('MainCtrl', [
       post.upvotes += 1;
     }
   }]);
+  app.factory('posts', [function(){
+    var o = {
+      posts: [
+        {title: 'post 1', upvotes: 23},
+        {title: 'post 2', upvotes: 55},
+        {title: 'post 3', upvotes: 11},
+        {title: 'post 4', upvotes: 41},
+        {title: 'post 5', upvotes: 36}
+      ]};
+    return o;
+  }])
